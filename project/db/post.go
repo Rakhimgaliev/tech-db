569aa44f8b5d2937a5ad2d9c00ae9595d050bffa
boss <rakhimgaliev56@gmail.com>
2019-04-22 02:45:41 +0300

    i can create posts

--- project/db/post.go
+++ project/db/post.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"database/sql"
 	"errors"
 	"fmt"
 	"strconv"
@@ -12,7 +13,9 @@ import (
 )
 
 var (
-	ErrorThreadNotFound = errors.New("Forum already exists")
+	ErrorThreadNotFound       = errors.New("Forum already exists")
+	ErrorPostCreateConflict   = errors.New("Post create conflict")
+	ErrorPostCreateBadRequest = errors.New("Post Create Bad Request")
 )
 
 const (
@@ -24,12 +27,12 @@ const (
 	`
 
 	createPostsQueryStart = `
-		INSERT INTO post (forum, author, created, message, edited, parent, thread)
+		INSERT INTO post (forum, userNickname, created, message, edited, parent, thread)
 		VALUES 
 	`
 
 	createPostsQueryEnd = `
-		RETURNING id, author, created, edited, message, parent, thread, forum
+		RETURNING id, userNickname, created, edited, message, parent, thread, forum
 	`
 
 	createForumUsersQueryStart = `
@@ -38,11 +41,29 @@ const (
 	`
 
 	createForumUsersQueryEnd = `
-		ON CONFLICT ON CONSTRAINT unique_forum_user DO NOTHING
+		ON CONFLICT ON CONSTRAINT uniqueForumUser DO NOTHING
+	`
+
+	createWithCheckParentID = `(
+		SELECT (
+			CASE WHEN 
+			EXISTS(SELECT 1 from post p where p.id=%v and p.thread=%v)
+			THEN %v ELSE -1 END)
+		)
+	`
+
+	updateForumPostCountByThreadId = `
+		UPDATE forum f SET postCount = postCount + $1
+		FROM thread t
+		WHERE t.forum = f.slug AND t.id = $2
 	`
 )
 
 func CreatePosts(conn *pgx.ConnPool, threadIdOrSlag string, posts *models.Posts) error {
+	if len(*posts) == 0 {
+		return ErrorPostCreateBadRequest
+	}
+
 	forumSlug, threadId, err := getForumSlugAndThreadIdByThreadSlugOrId(conn, threadIdOrSlag)
 	if err != nil {
 		return err
@@ -53,24 +74,91 @@ func CreatePosts(conn *pgx.ConnPool, threadIdOrSlag string, posts *models.Posts)
 		return err
 	}
 
-	err = insertPosts(transaction, threadId, posts, forumSlug)
+	_, err = transaction.Exec("SET LOCAL synchronous_commit TO OFF")
 	if err != nil {
+		if txErr := transaction.Rollback(); txErr != nil {
+			return txErr
+		}
+		return err
+	}
 
+	err = createPosts(transaction, threadId, posts, forumSlug)
+
+	if err != nil {
+		if txErr := transaction.Rollback(); txErr != nil {
+			return txErr
+		}
+
+		if pqError, ok := err.(pgx.PgError); ok {
+			switch pqError.Code {
+			case PgxErrorForeignKeyViolation:
+				if pqError.ConstraintName == "post_parent_id_fkey" {
+					return ErrorPostCreateConflict
+				}
+				if pqError.ConstraintName == "post_author_fkey" {
+					return ErrorUserNotFound
+				}
+			}
+		}
+		return err
+	}
+
+	if err = transaction.Commit(); err != nil {
+		return err
 	}
 
 	return nil
 }
 
-func insertPosts(transaction *pgx.Tx, threadId int32, posts *models.Posts, forumSlug string) error {
-	if len(*posts) == 0 {
-		return nil
-	}
-
+func createPosts(transaction *pgx.Tx, threadId int32, posts *models.Posts, forumSlug string) error {
 	postsArgs := make([]interface{}, 0)
 	forumUserArgs := make([]interface{}, 0)
-	insertPostsQuery, insertForumUserQuery := generatePostsQuery(forumSlug, threadId, posts, &postsArgs, &forumUserArgs)
 
-	return nil
+	createPostsQuery, createForumUserQuery := generatePostsQuery(forumSlug, threadId, posts, &postsArgs, &forumUserArgs)
+
+	*posts = nil
+	rows, err := transaction.Query(*createPostsQuery, postsArgs...)
+	if err != nil {
+		return err
+	}
+	for rows.Next() {
+		post := &models.Post{}
+		err := scanPostRows(rows, post)
+		if err != nil {
+			rows.Close()
+			return err
+		}
+
+		*posts = append(*posts, post)
+	}
+
+	if err := rows.Err(); err != nil {
+		rows.Close()
+		return err
+	}
+
+	rows.Close()
+
+	_, err = transaction.Exec(updateForumPostCountByThreadId, len(*posts), threadId)
+	if err != nil {
+		return err
+	}
+
+	_, err = transaction.Exec(*createForumUserQuery, forumUserArgs...)
+	return err
+}
+
+func scanPostRows(r *pgx.Rows, post *models.Post) error {
+	parent := sql.NullInt64{}
+	err := r.Scan(&post.Id, &post.Author, &post.Created, &post.IsEdited,
+		&post.Message, &parent, &post.Thread, &post.Forum)
+
+	if parent.Valid {
+		post.Parent = parent.Int64
+	} else {
+		post.Parent = 0
+	}
+	return err
 }
 
 func generatePostsQuery(forumSlug string, threadId int32, posts *models.Posts,
@@ -84,7 +172,6 @@ func generatePostsQuery(forumSlug string, threadId int32, posts *models.Posts,
 	for idx, post := range *posts {
 		createValues = formCreateValuesId(post.Author, post.Created, post.Message, threadId,
 			post.IsEdited, post.Parent, post.Thread, idx*5+1, postsArgs, forumSlug)
-
 		createUserValues = formCreateUserValues(post.Author, idx*2+1, forumUserArgs, forumSlug)
 
 		if idx != 0 {
@@ -111,8 +198,61 @@ func generatePostsQuery(forumSlug string, threadId int32, posts *models.Posts,
 
 func formCreateValuesId(author string, created time.Time, message string, threadId int32, isEdited bool, parent int64,
 	postThreadId int32, placeholderStart int, valuesArgs *[]interface{}, forumSlug string) string {
-	res := ""
-	return res
+	values := "("
+	valuesArr := []string{}
+	placeholder := placeholderStart
+
+	valuesArr = append(valuesArr, fmt.Sprintf(`'%v'`, forumSlug))
+
+	valuesArr = append(valuesArr, fmt.Sprintf("$%v", placeholder))
+	placeholder++
+
+	if author == "" {
+		*valuesArgs = append(*valuesArgs, "NULL")
+	} else {
+		*valuesArgs = append(*valuesArgs, author)
+	}
+
+	valuesArr = append(valuesArr, fmt.Sprintf("$%v", placeholder))
+	placeholder++
+
+	if created.IsZero() {
+		*valuesArgs = append(*valuesArgs, "now()")
+	} else {
+		*valuesArgs = append(*valuesArgs, created)
+	}
+
+	valuesArr = append(valuesArr, fmt.Sprintf("$%v", placeholder))
+	placeholder++
+
+	if message == "" {
+		*valuesArgs = append(*valuesArgs, "NULL")
+	} else {
+		*valuesArgs = append(*valuesArgs, message)
+	}
+
+	valuesArr = append(valuesArr, fmt.Sprintf("$%v", placeholder))
+	placeholder++
+
+	*valuesArgs = append(*valuesArgs, isEdited)
+
+	if parent == 0 {
+		valuesArr = append(valuesArr, fmt.Sprint("(NULL)"))
+	} else {
+		valuesArr = append(valuesArr, fmt.Sprintf(createWithCheckParentID, parent, threadId, parent))
+	}
+
+	valuesArr = append(valuesArr, fmt.Sprintf("$%v", placeholder))
+	if postThreadId == 0 {
+		*valuesArgs = append(*valuesArgs, threadId)
+	} else {
+		*valuesArgs = append(*valuesArgs, postThreadId)
+	}
+
+	values += strings.Join(valuesArr, ", ")
+	values += ")"
+
+	return values
 }
 
 func formCreateUserValues(author string, placeholder int, args *[]interface{}, forumSlug string) string {
@@ -135,7 +275,7 @@ func getForumSlugAndThreadIdByThreadSlugOrId(conn *pgx.ConnPool, threadIdOrSlug 
 		}
 		return forumSlug, int32(threadId), nil
 	}
-	err := conn.QueryRow(getForumSlugAndThreadIdByThreadSlug, threadIdOrSlug).Scan(&threadId, &forumSlug)
+	err := conn.QueryRow(getForumSlugAndThreadIdByThreadSlug, threadIdOrSlug).Scan(&forumSlug, &threadId)
 	if err != nil {
 		if err == pgx.ErrNoRows {
 			return forumSlug, threadId, ErrorThreadNotFound
--- project/handler/Post.go
+++ project/handler/Post.go
@@ -2,18 +2,43 @@ package handler
 
 import (
 	"encoding/json"
+	"log"
 
 	"github.com/Rakhimgaliev/tech-db-forum/project/db"
 	"github.com/Rakhimgaliev/tech-db-forum/project/models"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 )
+
+func BindJSON(context *gin.Context, obj interface{}) error {
+	if err := binding.JSON.Bind(context.Request, obj); err != nil {
+		context.Error(err).SetType(gin.ErrorTypeBind)
+		return err
+	}
+	return nil
+}
 
 func (h handler) CreatePosts(context *gin.Context) {
 	posts := models.Posts{}
 
-	context.BindJSON(posts)
+	err := BindJSON(context, &posts)
 
-	err := db.CreatePosts(h.db, context.Param("slug_or_id"), &posts)
+	err = db.CreatePosts(h.conn, context.Param("slug_or_id"), &posts)
+	log.Println(err, posts)
+	if err != nil {
+		switch err {
+		case db.ErrorPostCreateBadRequest:
+			postsJSON, _ := json.Marshal(posts)
+			context.Data(201, "application/json", postsJSON)
+		case db.ErrorThreadNotFound, db.ErrorForumNotFound:
+			context.JSON(404, err)
+		case db.ErrorPostCreateConflict:
+			context.JSON(409, err)
+		default:
+			context.JSON(500, err)
+		}
+		return
+	}
 
 	postsJSON, _ := json.Marshal(posts)
 	context.Data(201, "application/json", postsJSON)
